43aefdcc660bd59f057404434a03ee7b2fccfb61
Cato Nano <catonano@gmail.com>
2014-03-07 15:47:15 +0100

    namespace definition

--- src/playground_rebuild/operations.clj
+++ src/playground_rebuild/operations.clj
@@ -1,4 +1,8 @@
-(ns playground-rebuild.operations)
+(ns playground-rebuild.operations
+  (:refer-clojure :exclude [* - + == /])
+  (:use clojure.core.matrix)
+  (:use clojure.core.matrix.operators))
+  )
 
 (defn coremult [a-vector]
   "it multiplies a column vector for itself"
