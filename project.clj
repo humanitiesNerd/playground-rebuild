75efc57555ea058eccdef13f1d0324053d1fc14c
Cato Nano <catonano@gmail.com>
2014-03-07 18:13:54 +0100

    midje-cascalog as a dev dependency again. Now, unexplicably it works

--- project.clj
+++ project.clj
@@ -5,8 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [net.mikera/core.matrix "0.20.0"]
-                 [cascalog/midje-cascalog "2.0.0"]
+
                  [cascalog/cascalog-core "2.0.0"]]
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.4"]
                                   [org.apache.hadoop/hadoop-core "1.2.1"]
-                                  ]}})
+                                  [cascalog/midje-cascalog "2.0.0"] ] }})
