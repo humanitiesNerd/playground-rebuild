2b830dcece556aa78b80e8862897a35e28ab751f
Cato Nano <catonano@gmail.com>
2014-03-09 15:42:50 +0100

    added cascalog and hadoop

--- project.clj
+++ project.clj
@@ -5,5 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [net.mikera/core.matrix "0.20.0"]
+                 [cascalog/cascalog-core "2.0.0"]
+                 [cascalog/cascalog-more-taps "2.0.0" ]
                  ]
-  :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.4"]]}})
+  :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.4"]
+                                  [org.apache.hadoop/hadoop-core "1.1.2"]]}})
