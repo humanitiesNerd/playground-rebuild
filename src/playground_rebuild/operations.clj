e67e53a3d2ba28adbb5a952126dbf56b46c73523
Cato Nano <catonano@gmail.com>
2014-03-15 20:56:13 +0100

    finished produce-A and produce-b. With unit tests. Incredible

--- src/playground_rebuild/operations.clj
+++ src/playground_rebuild/operations.clj
@@ -7,13 +7,26 @@
         )
   )
 
+(defn test-data []
+  (pig/return [[3 5 3 1] [2 3 4 1]]))
+
 
 (defn coremult [a-vector]
-  "it multiplies a column vector for itself"
+  "it multiplies a column vector for itself, but without the last digit, assumed to be belonging to the y column"
   (let [without-y (subvec a-vector 0 (- (count a-vector) 1))
         my-vector (m/matrix [without-y])]
     (m/mmul (m/transpose my-vector) my-vector)))
 
+
 (defn produce-A [data]
   (pig/reduce m/add
               (pig/map coremult (data))))
+
+
+(defn coremult2 [a-vector]
+  (let [y (peek a-vector)
+        without-y (subvec a-vector 0 (- (count a-vector) 1))]
+    (m/mul y without-y)))
+
+(defn produce-b [data]
+  (pig/reduce m/add (pig/map coremult2 (data))))
--- test/playground_rebuild/operations_test.clj
+++ test/playground_rebuild/operations_test.clj
@@ -6,27 +6,31 @@
   )
 
 
-(defn test-data []
-  (pig/return [[3 5 1] [2 3 1]]))
 
 
 (deftest test-my-data
   (let [data (test-data)]
     (is (= (pig/dump data)
-           [[3 5 1] [2 3 1] ]))))
+           [[3 5 3 1] [2 3 4 1] ]))))
 
 (deftest test-produce-A
   (let [calculated-data (pig/dump (produce-A test-data))
-        expected-data [[[[13 21] [21 34]]]]]
+        expected-data [[[[13 21 17] [21 34 27] [17 27 25]]]]]
     (is (= calculated-data expected-data)))
   )
 
-(comment
-  -----------------------
-[[9 15] [15 25]]
-[[4 6] [6 9]]
------------------------
+(deftest test-produce-b
+  (let [calculated-data (pig/dump (produce-b test-data))
+        expected-data [[[5 8 7]]]]
+    (is (= calculated-data expected-data))))
 
-[[[[13 21] [21 34]]]]
+(comment
+  output of (produce-A test-data)
+-----------------------
+[[[[13 21 17] [21 34 27] [17 27 25]]]]
+-----------------------
+output of (produce-b  test-data)
+[[[5 8 7]]]
+
 
 )
